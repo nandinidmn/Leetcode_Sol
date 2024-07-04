c20205dc3b2a138aee2209b35ba999cf2c7a0a71
nandinidmn <136487988+nandinidmn@users.noreply.github.com>
2024-07-04 10:30:41 +0530

    Time: 1 ms (99.34%), Space: 41.5 MB (98%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -1,25 +1,22 @@
 class Solution {
     public boolean isValid(String word) {
-        if(word.length()<=2)return false;
-
-        String v="aeiouAEIOU";
-        int vc=0;
-        int cc=0;
+        if(word.length()<3)return false;
+        int v=0,ct=0,d=0;
         for(int i=0;i<word.length();i++){
             char c=word.charAt(i);
-            if(Character.isLetter(c)){
-                if(v.contains(Character.toString(c))){
-                    vc++;
-                }else{
-                    cc++;
-                }
+            if(c=='a' || c=='A' || c=='e' || c=='E' || c=='i' || c=='I' || c=='o' || c=='O' ||c=='u' || c=='U' ){
+                v++;
             }
-            else if(c=='@' || c=='#' || c=='$'){
+            else if(c>=65 && c<=90 || c>=97 && c<=122){
+                ct++;
+            }
+            else if(c>=48 && c<=57){
+                d++;}
+                else {
                 return false;
             }
         }
-        if(vc>=1 && cc>=1)return true;
-        else
-        return false;
+        if(ct>=1 && v>=1)return true;
+        else return false;
     }
 }
