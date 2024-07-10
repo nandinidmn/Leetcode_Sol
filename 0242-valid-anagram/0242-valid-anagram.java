aa97a46ce4661c02a6be4411c9a24cd4ca26741b
nandinidmn <136487988+nandinidmn@users.noreply.github.com>
2024-07-10 14:17:16 +0530

    Time: 4 ms (70.6%), Space: 43.2 MB (68.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())return false;
       int [] a=new int[128];
       for(int i=0;i<s.length();i++){
         a[s.charAt(i)]++;
