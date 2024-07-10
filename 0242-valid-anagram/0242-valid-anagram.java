eafaa23d86f0f85f40e6ecf7a590370400648a83
nandinidmn <136487988+nandinidmn@users.noreply.github.com>
2024-07-10 14:17:53 +0530

    Time: 5 ms (53.76%), Space: 43.4 MB (63.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,11 +3,9 @@ class Solution {
         if(s.length()!=t.length())return false;
       int [] a=new int[128];
       for(int i=0;i<s.length();i++){
-        a[s.charAt(i)]++;
+        a[s.charAt(i)]++; a[t.charAt(i)]--;
       }
-      for(int i=0;i<t.length();i++){
-        a[t.charAt(i)]--;
-      }
+     
       for(int i=0;i<a.length;i++){
         if(a[i]!=0)return false;
       }
