59700954db217fbe9bcc3f7d6feea49efaf4331d
nandinidmn <136487988+nandinidmn@users.noreply.github.com>
2024-07-16 10:06:36 +0530

    Time: 260 ms (63.64%), Space: 41.9 MB (77.89%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,7 +1,7 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         int minrep=(int)Math.ceil(b.length()/a.length());
-        for(int i=0;i<=minrep+2;i++){
+        for(int i=minrep;i<=minrep+2;i++){
            String rep=a.repeat(i);
            if(rep.contains(b))return i;
         }
