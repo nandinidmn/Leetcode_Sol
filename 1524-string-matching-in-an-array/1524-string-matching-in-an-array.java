e759e309a70007ccae7fd61f8c30886f04d9773c
nandinidmn <136487988+nandinidmn@users.noreply.github.com>
2024-07-16 14:07:53 +0530

    Time: 5 ms (46.14%), Space: 42.5 MB (7.83%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -4,7 +4,7 @@ class Solution {
     //    Set<String> ans=new HashSet<>();
         for(int i=0;i<words.length;i++){
         for(int j=0;j<words.length;j++){
-            if(i!=j && words[i]!=words[j] && words[j].contains(words[i])){
+            if(i!=j && words[j].contains(words[i])){
                 if(!a.contains(words[i])){
                     a.add(words[i]);
                 }
