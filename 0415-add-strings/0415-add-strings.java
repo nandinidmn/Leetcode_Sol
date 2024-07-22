5c40394189dd8500149c1f3d9e8cfd2563c7bddd
nandinidmn <136487988+nandinidmn@users.noreply.github.com>
2024-07-22 10:07:42 +0530

    Time: 1 ms (100%), Space: 42.4 MB (68.54%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,8 +1,17 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        java.math.BigInteger a=new java.math.BigInteger(num1);
-        java.math.BigInteger b=new java.math.BigInteger(num2);
-        a=a.add(b);
-        return a.toString();
+       int i=num1.length()-1;
+       int j=num2.length()-1;
+       int c=0;
+       StringBuilder st=new StringBuilder();
+       while(i>=0 || j>=0 || c>0){
+        int a=(i>=0)?num1.charAt(i)-'0':0;
+        int b=(j>=0)?num2.charAt(j)-'0':0;
+        int s=a+b+c;
+        c=s/10;
+        st=st.append(s%10);
+        i--;j--;
+       }
+       return st.reverse().toString();
     }
 }
