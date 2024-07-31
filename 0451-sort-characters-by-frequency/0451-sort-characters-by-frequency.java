9546e6616469d1eb7593abf9daa66e992a4aebc0
nandinidmn <136487988+nandinidmn@users.noreply.github.com>
2024-07-31 10:28:25 +0530

    Time: 385 ms (9.04%), Space: 45.7 MB (27.93%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,31 +1,25 @@
 class Solution {
     public String frequencySort(String s) {
-       HashMap<Character,Integer> h=new HashMap<>();
-       for(int i=0;i<s.length();i++){
-        h.put(s.charAt(i),h.getOrDefault(s.charAt(i),0)+1);
-       }
-
-
-       HashMap<Character, Integer> temp
-            = h.entrySet()
-                  .stream()
-                  .sorted((i1, i2)
-                              -> i1.getValue().compareTo(
-                                  i2.getValue()))
-                  .collect(Collectors.toMap(
-                      Map.Entry::getKey,
-                      Map.Entry::getValue,
-                      (e1, e2) -> e1, LinkedHashMap::new));
-       String ans="";
-      
-       for(Map.Entry<Character,Integer> e:temp.entrySet()){
-        int n=e.getValue();
-        while(n!=0){
-            ans+=e.getKey();
-            n--;
+        HashMap<Character,Integer> h=new HashMap<>();
+        for(char c:s.toCharArray()){
+            h.put(c,h.getOrDefault(c,0)+1);
         }
-       }
-       StringBuilder k=new StringBuilder(ans);
-       return k.reverse().toString();
+        String ans="";
+        while(!h.isEmpty()){
+          int max=Integer.MIN_VALUE;
+          char c='\0';
+          for(Map.Entry<Character,Integer> e:h.entrySet()){
+            if(max<e.getValue()){
+                max=e.getValue();
+                c=e.getKey();
+            }
+          }
+          for(int i=0;i<max;i++){
+            ans+=c;
+          }
+          h.remove(c);
+          
+        }
+        return ans;
     }
 }
