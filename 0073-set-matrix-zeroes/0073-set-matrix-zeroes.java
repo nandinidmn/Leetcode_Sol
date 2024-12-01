1eab75c62357046737b9bd86dad8b80fff9747b6
nandinidmn <136487988+nandinidmn@users.noreply.github.com>
2024-12-01 16:15:16 +0530

    Time: 1 ms (76.48%), Space: 45.4 MB (84.34%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean[] r=new boolean[matrix.length],c=new boolean[matrix[0].length] ;
+       for(int i=0;i<matrix.length;i++){
+        for(int j=0;j<matrix[0].length;j++){
+            if(matrix[i][j]==0){
+                r[i]=true;
+                c[j]=true;
+            }
+        }}
+        for(int i=0;i<matrix.length;i++){
+        for(int j=0;j<matrix[0].length;j++){
+            if(r[i] || c[j]){
+                matrix[i][j]=0;
+            }
+        }
+       }
+
+    }}
